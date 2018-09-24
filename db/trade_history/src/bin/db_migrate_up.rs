b34ffe59e43f12b07deaba09773a70d318b73a8c
Stephan Luther <kvsari@gmail.com>
2018-09-24 23:00:12 +0900

    Add retry loop to migrator

--- db/trade_history/src/bin/db_migrate_up.rs
+++ db/trade_history/src/bin/db_migrate_up.rs
@@ -7,15 +7,25 @@ extern crate postgres;
 extern crate trade_history;
 
 use std::env::var;
+use std::{thread, time};
 
 pub fn main() {
     dotenv::dotenv().ok();
     env_logger::init();
     let db_url = var("DATABASE_URL").expect("DATABASE_URL env var not set.");
+    let wait = time::Duration::from_secs(30);
 
-    info!("Connecting to DB.");
-    let connection = postgres::Connection::connect(db_url, postgres::TlsMode::None)
-        .expect("Can't connect to database.");
+    let connection = loop {
+        info!("Connecting to DB.");
+        match postgres::Connection::connect(db_url.clone(), postgres::TlsMode::None) {
+            Ok(c) => break c,
+            Err(e) => {
+                warn!("Failed to connect to DB: {}", &e);
+                info!("Retrying in {:?}", &wait);
+                thread::sleep(wait);
+            }
+        }
+    };
     
     info!("Setting up migrations.");
     let migrator = trade_history::migration::setup(&connection)
